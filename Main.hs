7be055e2ec09f9134a3364f2b5aa80ca953e031b
Dmitry Andreevich <dmand@yandex.ru>
2014-01-07 04:08:20 +0300

    Refactor and fix symmetry issue

--- Main.hs
+++ Main.hs
@@ -4,35 +4,35 @@ import Graphics.Gloss
 import Snowflakes
 import System.Random
 
-glossifySnowflake :: Snowflake -> Point -> Picture
-glossifySnowflake snowy startPos =
+drawSnowflake :: Snowflake -> Point -> Picture
+drawSnowflake snowy startPos =
   let expanded = expandPattern snowy
-      segmentLen = 10
-      folding (sx, sy) (Segment angle) =
-        let nx = sx + segmentLen * (cos angle)
-            ny = sy + segmentLen * (sin angle)
-        in (nx, ny)
-      points = scanl folding startPos expanded
-      lines = [Line [s, e] | (s, e) <- zip points (tail points)]
-  in Color white $ Pictures lines
-
-
-printCalculations :: Snowflake -> Point -> IO ()
-printCalculations snowy startPos = do
-  let expanded = expandPattern snowy
-      segmentLen = 10
+      segmentLen = 1
       folding (sx, sy) (Segment angle) =
         let nx = fromInteger $ round $ sx + segmentLen * (cos angle)
             ny = fromInteger $ round $ sy + segmentLen * (sin angle)
         in (nx, ny)
       points = scanl folding startPos expanded
       lines = [Line [s, e] | (s, e) <- zip points (tail points)]
-  putStrLn (show lines)
+  in Color white $ Pictures lines
+
+printCalculations :: Snowflake -> Point -> IO ()
+printCalculations snowy startPos = do
+  let expanded = expandPattern snowy
+      segmentLen = 1
+      folding (sx, sy) (Segment angle) =
+        let nx = fromInteger $ round $ sx + segmentLen * (cos angle)
+            ny = fromInteger $ round $ sy + segmentLen * (sin angle)
+        in (nx, ny)
+      points = scanl folding startPos expanded
+      lines = [Line [s, e] | (s, e) <- zip points (tail points)]
+  putStrLn $ show expanded
+  putStrLn $ show lines
 
 main = do
   gen <- getStdGen
-  let (snowy, _) = randomSnowflake 4 2 gen
-  putStrLn $ show snowy
-  printCalculations snowy (400, 400)
-  let snowflakePic = glossifySnowflake snowy (400, 400)
-  display (InWindow "Nice Window" (800, 800) (3, 3)) black snowflakePic
+  let snowy = randomSnowflake 8 200 gen
+  --putStrLn $ show snowy
+  --printCalculations snowy (0, 0)
+  let snowflakePic = drawSnowflake snowy (0, 0)
+  display (InWindow "Nice Window" (500, 500) (10, 10)) black snowflakePic
--- Snowflakes.hs
+++ Snowflakes.hs
@@ -2,38 +2,36 @@ module Snowflakes where
 
 import System.Random
 
-data Segment = Segment Float deriving (Show)
+data Segment = Segment Float
+             deriving (Show)
 type Pattern = [Segment]
 
 data Snowflake = Snowflake {
-  factor :: Integer, -- factor is the count of segments in pattern
+  factor :: Int, -- factor is the count of segments in pattern
   pattern :: Pattern
 } deriving (Show)
 
-randomPattern :: (RandomGen g) => Integer -> Integer -> g -> (Pattern, g)
-randomPattern factor size gen = randomPattern' [] factor size gen
+randomPattern :: (RandomGen g) => Int -> Int -> g -> Pattern
+randomPattern factor size gen =
+  let zeroToNs = take size $ randomRs (0, factor - 1) gen
+      angles = [ (pi :: Float) * (fromIntegral n) / (fromIntegral factor)
+                 | n <- zeroToNs]
+      segments = map Segment angles
+  in segments 
 
-randomPattern' :: (RandomGen g) => Pattern -> Integer -> Integer -> g -> (Pattern, g)
-randomPattern' acc factor 0 gen = (acc, gen)
-randomPattern' acc factor size gen =
-  let (n, newGen) = randomR (0, factor - 1) gen
-      angle = (pi :: Float) * 2 * ((fromInteger n) / (fromInteger factor))
-      segment = Segment angle
-  in randomPattern' (segment : acc) factor (size - 1) newGen
-
-randomSnowflake :: (RandomGen g) => Integer -> Integer -> g -> (Snowflake, g)
+randomSnowflake :: (RandomGen g) => Int -> Int -> g -> Snowflake
 randomSnowflake factor size gen =
-  let (pattern, newGen) = randomPattern factor size gen
+  let pattern = randomPattern factor size gen
       snowflake = Snowflake { pattern = pattern, factor = factor }
-  in (snowflake, newGen)
+  in snowflake
 
--- expand pattern to full snowflake
-expandPattern :: Snowflake -> [Segment]
+-- expand pattern to full snowflakeexpandPattern :: Snowflake -> [Segment]
 expandPattern snowy =
   let fac = factor snowy
       pat = pattern snowy
   in [newSegment |
-        i <- [1..fac],
+        part <- [1..fac],
         segment <- pat,
         let Segment angle = segment,
-        let newSegment = Segment (angle * (fromIntegral i))]
+        let q = 2 * pi * ((fromIntegral part) / (fromIntegral fac)),
+        let newSegment = Segment (q + angle)]
